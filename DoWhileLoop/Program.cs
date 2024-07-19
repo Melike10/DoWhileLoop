466f5a0c735f2747f57bf0d8e8fa88915ee3c680
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-07-19 17:38:23 +0300

    Add project files.

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿int sayac = 0;
+int limit;
+do
+{
+
+    Console.WriteLine("Litfen bir limit Giriniz");
+    limit = int.Parse(Console.ReadLine());
+    if (limit >= 0) {
+        Console.WriteLine("Ben bir Patika'lıyım.");
+    }
+   
+    sayac++;
+
+
+} while (sayac <= limit);
+/* do-while döngüsü en az bir kez kodların çalışmasını garanti ederken while koşula uyma durumunda sadece kodları çalıştırır.*/
